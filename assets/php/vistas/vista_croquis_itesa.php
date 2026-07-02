1af2d17fa9c0efd31221704c0bb1f880b0a11686
diegocorvar <diegoalonso.DACV@gmail.com>
2026-07-02 17:48:14 -0600

    feat: crear archivo vista_croquis_itesa y crear carpeta assets/php/vistas

---
+++ assets/php/vistas/vista_croquis_itesa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Croquis Itesa</title>
+
+    <link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body>
+    <main>
+        <?php include('../componentes/menu_desplegable_izquierdo.php'); ?>
+        <?php include('../componentes/barra_busqueda.php'); ?>
+    </main>
+</body>
+</html>
